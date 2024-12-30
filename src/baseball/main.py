745a3ada1dce6d13e7276174289d49709d90ab16
김성규 <kof9e1@naver.com>
2024-12-31 02:12:59 +0900

    코멘트 부분 수정

--- src/baseball/main.py
+++ src/baseball/main.py
@@ -5,7 +5,7 @@ def player_input() :
     player_number = input("숫자를 입력해주세요: ")
     if len(player_number) != 3 or not player_number.isdigit():
         raise ValueError("입력은 3자리 숫자여야 합니다.")
-            
+
     elif len(set(player_number)) != 3:
         raise ValueError("입력 값의 각 자리는 서로 달라야 합니다.")
     return player_number
@@ -13,7 +13,7 @@ def player_input() :
 
 def game_comp(com_number, player_number) :
     strike, ball, index = 0, 0, 0
-  
+
     for i in com_number :
         if int(player_number[index]) == i :
             strike += 1
@@ -21,7 +21,6 @@ def game_comp(com_number, player_number) :
             ball += 1
         index += 1
     return strike, ball
-
 
 def game_print(com_number) :
     while True:
@@ -31,11 +30,11 @@ def game_print(com_number) :
             print("낫싱")
             continue
         elif strike == 3 :
-            print(strike,"스트라이크")
-            print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            print(str(strike)+"스트라이크")
+            print("게임 종료")
             break
         else:
-            print(ball,"볼"," ",strike,"스트라이크")
+            print(str(ball)+"볼", str(strike)+"스트라이크")
             continue
         
 def main(): 
@@ -43,14 +42,14 @@ def main():
         com_number = random.sample(range(1,10),3)
         print("숫자 야구 게임을 시작합니다.")
         game_print(com_number)
-    
-        
+
+
         result = input("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         if result == '1':
             main()
         elif result == '2':
             print("게임을 종료합니다.")
-            exit()
+            return
         else:
             raise ValueError("잘못된 입력입니다. 1 또는 2를 입력하세요.")
             
