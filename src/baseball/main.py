804110fba4642b36a6d21f617a9c2e244f39e20e
akran <nhlk123@naver.com>
2025-02-04 23:45:00 +0900

    완성

--- src/baseball/main.py
+++ src/baseball/main.py
@@ -22,9 +22,64 @@ def pick():
     return print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
 
 def main():
-    
-    
-    
+    print('숫자 야구 게임을 시작합니다')
+    while 1:
+
+        strike=0
+        ball=0
+        l=[]
+        computer=randomnumber()
+
+
+
+
+        while 1:
+        
+            computer1=computer.copy()
+            
+            strike=0
+            ball=0
+            l=[]
+            a=input('숫자를 입력해주세요 :')
+            
+            if len(a) !=3 :
+                raise ValueError
+                    
+            for r in range(len(a)) :
+                l.append(int(a[r]))
+            
+            for i in range(len(computer)-1,-1,-1) :
+                if computer[i] == l[i] :
+                    strike+=1
+                    computer.pop(i)
+                    l.pop(i)
+                    
+                    
+            
+            for k in range(len(l)) :
+                if l[k] in computer :
+                    ball+=1
+            
+            computer=computer1  
+
+            if ball==0 and strike ==0 :
+                nothing()
+            elif strike==3 :
+                onlystrike(strike)
+                print('게임 종료')
+                break
+
+            elif ball==0 :
+                onlyball(ball)
+            elif strike==0 :
+                onlystrike(strike)
+            elif ball!=0 and strike!=0 :
+                ballandstrike(ball,strike)
+        press=input()
+        if press!='1':
+            break
+
+   
     
     
     # 프로그램의 메인 로직을 여기에 구현
