29aee5cc392028baa4b37916b8496b27aecfa688
AI-WonYJ <yjason7587@naver.com>
2025-02-05 03:00:00 +0900

    [#4] Add: a function that repeats the process from 1 to 3 and the game ends when all 3 numbers are guessed correctly

--- src/baseball/main.py
+++ src/baseball/main.py
@@ -27,6 +27,13 @@ def check_input(get, com):
         return "{0}볼".format(ball)
     else:
         return "{0}볼 {1}스트라이크".format(ball, strike)
+    
+def loop_check(com):
+    get = player_input()
+    result_check_input = check_input(get, com)
+    print(result_check_input)
+    if result_check_input != "3스트라이크":
+        loop_check(com)
 
 def main():
     """
@@ -36,8 +43,9 @@ def main():
     # 프로그램의 메인 로직을 여기에 구현
     print("숫자 야구 게임을 시작합니다.")
     computer = make_computer_num()
-    get = player_input()
-    print(check_input(get, computer))
+    print(computer)
+    loop_check(computer)
+    print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
 
 if __name__ == "__main__":
     # 프로그램이 직접 실행될 때만 main() 함수를 호출
