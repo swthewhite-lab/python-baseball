d6ed01abe3ecc3cc080aa5879323150b7a5b5afd
akran <nhlk123@naver.com>
2025-02-04 23:36:03 +0900

    출력 메세지

--- src/baseball/main.py
+++ src/baseball/main.py
@@ -1,6 +1,29 @@
 import random
+
+def randomnumber():
+   return random.sample(range(1, 10), 3)           
+
+def nothing():
+    return print("낫싱")
+
+def onlystrike(x):
+    return print("%d스트라이크"%x)
+
+def onlyball(x):
+    return print("%d볼"%x)
+
+def ballandstrike(x,y) :
+    return print("%d볼 %d스트라이크"%(x,y))
+
+def gamestart():
+    return print("숫자 야구 게임을 시작합니다")
+
+def pick():
+    return print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+
 def main():
-    computer = random.sample(range(1, 10), 3)
+    
+    
     
     
     
