2d6bdcee8c8f619afd1361bbb032e4582ebada85
하나 <hana@hanaui-noteubug.local>
2025-02-04 20:30:23 +0900

    feat: 입출력, 예외처리 기능

--- src/baseball/main.py
+++ src/baseball/main.py
@@ -1,9 +1,47 @@
+import random
+
 def main():
-    """
-    프로그램의 진입점 함수.
-    여기에서 전체 프로그램 로직을 시작합니다.
-    """
-    # 프로그램의 메인 로직을 여기에 구현
+    while True :
+        computer = generateNum()
+
+        print("숫자 야구 게임을 시작합니다.")
+        while(True) :
+            print("숫자를 입력해주세요 : ", end='')
+            user = input()
+            checkInput(user)
+            if computer == user :
+                break
+
+        
+        print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        select = input()
+        if select == "1" :
+            continue
+        elif select == "2" :
+            break
+        else :
+            raise ValueError
+
+
+def generateNum() :
+    num = random.sample(range(1,10), 3)
+    computer = "".join(map(str, num))
+    return computer
+
+def checkInput(user) :
+    if not user.isdigit() :
+        raise ValueError
+
+    num = int(user)
+    if num < 100 or num > 999 :
+        raise ValueError
+
+    check = list(user)
+    if len(set(check)) != 3 :
+        raise ValueError
+
+    return True
 
 if __name__ == "__main__":
     # 프로그램이 직접 실행될 때만 main() 함수를 호출
