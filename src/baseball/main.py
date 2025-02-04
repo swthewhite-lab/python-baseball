806c52b10ec53e40408f76246b3ed5125b0c5ad7
AI-WonYJ <yjason7587@naver.com>
2025-02-05 02:43:54 +0900

    Add: ability to ask player to enter 3 numbers

--- src/baseball/main.py
+++ src/baseball/main.py
@@ -4,6 +4,10 @@ def make_computer_num():
     computer = random.sample(range(1, 9), 3)
     return computer
 
+def player_input():
+    get_num = list(map(int, input("숫자를 입력해주세요 : ")))
+    return get_num
+
 def main():
     """
     프로그램의 진입점 함수.
@@ -13,9 +17,9 @@ def main():
     print("숫자 야구 게임을 시작합니다.")
     computer = make_computer_num()
     print(computer)
+    get = player_input()
+    print(get)
 
 if __name__ == "__main__":
     # 프로그램이 직접 실행될 때만 main() 함수를 호출
     main()
-
-    
