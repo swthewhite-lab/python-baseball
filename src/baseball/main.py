be41489dc2f6907893f21cf3db45e5f91ec6cc3e
akran <nhlk123@naver.com>
2025-02-04 22:51:34 +0900

    랜덤3자리수 생성

--- src/baseball/main.py
+++ src/baseball/main.py
@@ -1,8 +1,9 @@
+import random
 def main():
-    """
-    프로그램의 진입점 함수.
-    여기에서 전체 프로그램 로직을 시작합니다.
-    """
+    computer = random.sample(range(1, 10), 3)
+    
+    
+    
     # 프로그램의 메인 로직을 여기에 구현
 
 if __name__ == "__main__":
