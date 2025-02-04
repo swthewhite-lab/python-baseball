0ad080063975b052d532fc09b0a17c187dd8546e
AI-WonYJ <yjason7587@naver.com>
2025-02-04 17:28:12 +0900

    [#1] Add ability to generate 3 different random numbers from 1 to 9

--- src/baseball/main.py
+++ src/baseball/main.py
@@ -1,10 +1,21 @@
+import random
+
+def make_computer_num():
+    computer = random.sample(range(1, 9), 3)
+    return computer
+
 def main():
     """
     프로그램의 진입점 함수.
     여기에서 전체 프로그램 로직을 시작합니다.
     """
     # 프로그램의 메인 로직을 여기에 구현
+    print("숫자 야구 게임을 시작합니다.")
+    computer = make_computer_num()
+    print(computer)
 
 if __name__ == "__main__":
     # 프로그램이 직접 실행될 때만 main() 함수를 호출
     main()
+
+    
